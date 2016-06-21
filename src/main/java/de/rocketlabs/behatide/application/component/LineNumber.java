e148b4539d2e25b2de33f80a86baef14cfaf388f
Dennis Munchausen <dennis.munchausen@rocket-internet.de>
2016-06-21 12:31:30 +0200

    Textarea ui implementation

---
+++ src/main/java/de/rocketlabs/behatide/application/component/LineNumber.java
@@ -0,0 +1,50 @@
+package de.rocketlabs.behatide.application.component;
+
+import java.util.function.IntFunction;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+
+import org.reactfx.collection.LiveList;;
+import org.reactfx.value.Val;
+
+class LineNumber implements IntFunction {
+
+    private static final Insets DEFAULT_INSETS = new Insets(0.0, 5.0, 0.0, 5.0);
+    private static final Font DEFAULT_FONT =
+            Font.font("monospace", FontPosture.ITALIC, 13);
+
+
+    private final Val<Integer> nParagraphs;
+    private final IntFunction<String> format;
+
+    LineNumber(
+            ObservableList size,
+            IntFunction<String>format) {
+        nParagraphs = LiveList.sizeOf(size);
+        this.format = format;
+    }
+
+    @Override
+    public Node apply(int idx) {
+        Val<String> formatted = nParagraphs.map(n -> format(idx+1, n));
+
+        Label lineNo = new Label();
+        lineNo.setFont(DEFAULT_FONT);
+        lineNo.setPadding(DEFAULT_INSETS);
+        lineNo.getStyleClass().add("lineNumber");
+
+        lineNo.textProperty().bind(formatted.conditionOnShowing(lineNo));
+
+        return lineNo;
+    }
+
+    private String format(int x, int max) {
+        int digits = (int) Math.floor(Math.log10(max)) + 1;
+        return String.format(format.apply(digits), x);
+    }
+}
--- src/main/java/de/rocketlabs/behatide/application/configuration/components/storage/StateStorageManager.java
+++ src/main/java/de/rocketlabs/behatide/application/configuration/components/storage/StateStorageManager.java
@@ -4,10 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import de.rocketlabs.behatide.application.configuration.components.exceptions.StateStorageException;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,30 +26,37 @@ public class StateStorageManager {
         return instance;
     }
 
-    @Nullable
+    @NotNull
     public <T> T loadState(@NotNull Class<T> stateClass) {
         State state = stateClass.getAnnotation(State.class);
         if (state == null) {
             throw new StateStorageException("Given object misses @State annotation");
         }
+        T object = null;
         for (Storage storage : state.storages()) {
             StateStorage stateStorage = getStateStorage(storage.storageClass());
             if (stateStorage.hasState(state)) {
-                return deserializeData(stateClass, stateStorage.loadData(state));
+                object = deserializeData(stateClass, stateStorage.loadData(state));
+                break;
             }
         }
-        return null;
+        if (object == null) {
+            try {
+                object = stateClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new StateStorageException("State object could not be instantiated", e);
+            }
+        }
+        return object;
     }
 
     public <T extends StateStorage> StateStorage getStateStorage(Class<T> storageClass) throws StateStorageException {
         if (!storageClass.equals(StateStorage.class)) {
             if (!storageInstances.containsKey(storageClass)) {
                 try {
-                    Constructor<T> constructor = storageClass.getConstructor();
-                    constructor.setAccessible(true);
-                    T storage = constructor.newInstance();
+                    T storage = storageClass.newInstance();
                     storageInstances.put(storageClass, storage);
-                } catch (NoSuchMethodException | InstantiationException | InvocationTargetException | IllegalAccessException e) {
+                } catch (InstantiationException | IllegalAccessException e) {
                     throw new StateStorageException("Storage could not be instantiated", e);
                 }
             }
