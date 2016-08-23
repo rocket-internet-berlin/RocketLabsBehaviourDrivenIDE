573dcd86074cf051f4e5ac8c76ad52cdff4c11ff
Jakob Erdmann <jakob.erdmann@rocket-internet.de>
2016-08-23 17:28:29 +0200

    Changed key handling to suggested way from StyledTextArea documentation

--- src/main/java/de/rocketlabs/behatide/application/component/Editor.java
+++ src/main/java/de/rocketlabs/behatide/application/component/Editor.java
@@ -1,48 +1,55 @@
 package de.rocketlabs.behatide.application.component;
 
-import de.rocketlabs.behatide.application.keymanager.KeyManager;
 import de.rocketlabs.behatide.application.keymanager.listener.NewLineListener;
 import de.rocketlabs.behatide.application.keymanager.listener.TabListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination.Modifier;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.StyleSpans;
 import org.fxmisc.richtext.StyleSpansBuilder;
+import org.fxmisc.wellbehaved.event.EventHandlerHelper;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.IntFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static javafx.scene.input.KeyCode.ENTER;
+import static javafx.scene.input.KeyCode.TAB;
+import static javafx.scene.input.KeyCombination.SHIFT_ANY;
+import static org.fxmisc.wellbehaved.event.EventPattern.keyPressed;
+
 public class Editor extends VBox {
-    private static final String[] BEHAT_KEYWORDS = new String[]{
-            "Feature", "Scenario Outline", "Scenario Template", "Scenario", "Examples",
-            "Scenarios", "When", "Then", "Given", "And"
+
+    private static final CharSequence[] BEHAT_KEYWORDS = new String[]{
+        "Feature", "Scenario Outline", "Scenario Template", "Scenario", "Examples",
+        "Scenarios", "When", "Then", "Given", "And"
     };
 
     private static final String KEYWORD_REGEX = "(" + String.join("|", BEHAT_KEYWORDS) + ")";
     private static final Pattern PATTERN = Pattern.compile("(?<KEYWORD>" + KEYWORD_REGEX + ")");
     private static final String EDITOR_CODE = String.join("\n", "Feature: Setup_Text",
-            "",
-            "  Scenario: Behat IDE-editor Text Area",
-            "    When I start the application",
-            "    Then A new Stage gets created",
-            "    And I see a editor window",
-            "    When I click on the x of the Window",
-            "    Then The the window gets closed",
-            "    When Type any character",
-            "    Then the character should appear on the screen");
-
+                                                          "",
+                                                          "  Scenario: Behat IDE-editor Text Area",
+                                                          "    When I start the application",
+                                                          "    Then A new Stage gets created",
+                                                          "    And I see a editor window",
+                                                          "    When I click on the x of the Window",
+                                                          "    Then The the window gets closed",
+                                                          "    When Type any character",
+                                                          "    Then the character should appear on the screen"
+    );
 
     private static final Set<String> CSS_CLASS_KEYWORD = Collections.singleton("keyword");
     private static final Set<String> CSS_CLASS_DEFAULT = Collections.singleton("default");
-
-    private KeyManager keyManager;
 
     @FXML
     public CodeArea codeArea;
@@ -53,14 +60,25 @@ public class Editor extends VBox {
         fxmlLoader.setController(this);
         try {
             fxmlLoader.load();
-            registerKeyManager();
+            registerKeyCombinations();
 
             setDesignToCodeArea();
-            addEventsToCodeArea();
-
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private void setDesignToCodeArea() {
+        IntFunction<String> format = (digits -> "%" + (digits < 2 ? 2 : digits) + "d");
+        LineNumber ln = new LineNumber(codeArea.getParagraphs(), format);
+
+        codeArea.setParagraphGraphicFactory(ln);
+        codeArea.getStyleClass().add("editor-test-class");
+        codeArea.richChanges()
+                .filter(ch -> !ch.getInserted().equals(ch.getRemoved()))
+                .subscribe(change -> codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText())));
+        codeArea.replaceText(0, 0, EDITOR_CODE);
+        codeArea.setPrefSize(1000, 1000);
     }
 
     private static StyleSpans<Collection<String>> computeHighlighting(String text) {
@@ -76,32 +94,18 @@ public class Editor extends VBox {
         return spansBuilder.create();
     }
 
-    private Editor setDesignToCodeArea() {
-        IntFunction<String> format = (digits -> "%" + (digits < 2 ? 2 : digits) + "d");
-        LineNumber ln = new LineNumber(codeArea.getParagraphs(), format);
+    private void registerKeyCombinations() {
+        TabListener tabListener = new TabListener(codeArea);
+        registerKeyCombination(tabListener::handleEvent, TAB, SHIFT_ANY);
 
-        codeArea.setParagraphGraphicFactory(ln);
-        codeArea.getStyleClass().add("editor-test-class");
-        codeArea.richChanges()
-                .filter(ch -> !ch.getInserted().equals(ch.getRemoved()))
-                .subscribe(change -> codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText())));
-        codeArea.replaceText(0, 0, EDITOR_CODE);
-        codeArea.setPrefSize(1000, 1000);
-
-        return this;
+        NewLineListener enterListener = new NewLineListener(codeArea);
+        registerKeyCombination(enterListener::handleEvent, ENTER);
     }
 
-    private Editor addEventsToCodeArea() {
-        codeArea.setOnKeyPressed(e ->
-                keyManager.fireKeyEvent(e)
+    private void registerKeyCombination(Consumer<? super KeyEvent> action, KeyCode code, Modifier... modifiers) {
+        EventHandlerHelper.install(
+            codeArea.onKeyPressedProperty(),
+            EventHandlerHelper.on(keyPressed(code, modifiers)).act(action).create()
         );
-
-        return this;
-    }
-
-    private void registerKeyManager() {
-        keyManager = new KeyManager();
-        keyManager.addMapping(KeyCode.TAB, new TabListener(codeArea));
-        keyManager.addMapping(KeyCode.ENTER, new NewLineListener(codeArea));
     }
 }
